b7c0be3a433798853f825d6ba3fd99761a9385ae
Rick Barkley <rick_barkley@nuvola-networks.com>
2022-10-14 14:41:45 -0500

    donw case dna

--- app/models/dna_code.rb
+++ app/models/dna_code.rb
@@ -1,10 +1,13 @@
 class DnaCode < ApplicationRecord
 	belongs_to :account
 	before_save :modify_dna_string
+	validates_presence_of :account_id, :raw
+
 
 	def modify_dna_string
 		self.raw = self.raw.gsub("\n", "")
 		self.raw = self.raw.gsub("\r", "")
 		self.raw = self.raw.gsub(" ", "")
+		self.raw = self.raw.downcase
 	end
 end
